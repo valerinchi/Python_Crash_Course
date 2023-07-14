04360229d9b58b1d84225a65020f58951b80277f
Valeria <valethebesthola469@gmail.com>
2023-07-14 10:08:00 +0200

    Including population updates

--- population.py
+++ population.py
@@ -1,8 +1,5 @@
-def get_formatted_country(city, country, population=''):
-    if not isinstance(population, (int, float)):
-        raise TypeError("Population must be a number.")
-    if population:
-        the_country = f"{city}, {country}, {population}"
-    else:
-        the_country = f"{city}, {country}"
-    return the_country.title()
+def get_formatted_country(city, country, population=None):
+    full_country = f"{city}, {country}"
+    if population is not None:
+        full_country += f" - population {population}"
+    return full_country.title()
