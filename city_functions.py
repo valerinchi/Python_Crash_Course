b36f87dbe976d86c7b51f7af0b527ec065918ddb
Valeria <valethebesthola469@gmail.com>
2023-07-14 09:30:12 +0200

    City Functions try 2

---
+++ city_functions.py
@@ -0,0 +1,4 @@
+def get_city_country(city, country):
+    """Return a formatted string of the form City, Country."""
+    formatted_string = f"{city.title()}, {country.title()}"
+    return formatted_string
