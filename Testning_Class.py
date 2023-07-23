31a4fc824b7af3357f6a43fda8dff42e5b88b359
Valeria <valethebesthola469@gmail.com>
2023-07-23 08:04:11 +0200

    making survey 1

--- Testning_Class.py
+++ Testning_Class.py
@@ -1,1 +1,21 @@
 print("Lets upload these sentence on github :)")
+
+# A Variety of Assertions
+"""page 218"""
+
+# A Class to Test
+class AnonymousSurvey:
+    def __init__(self, question):
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        print(self.question)
+    
+    def store_response(self, new_response):
+        self.responses.append(new_response)
+    
+    def show_results(self):
+        print("Survey results:")
+        for response in self.responses:
+            print(f"- {response}")
